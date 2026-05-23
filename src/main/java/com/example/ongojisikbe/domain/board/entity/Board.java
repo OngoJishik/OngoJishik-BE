a2a008f5217ff14adab2f2102862173eedc3437f
bkh010914@naver.com <bkh010914@naver.com>
2026-05-23 23:38:15 +0900

    chore: 기초 디렉토리 정립

---
+++ src/main/java/com/example/ongojisikbe/domain/board/entity/Board.java
@@ -0,0 +1,35 @@
+package com.example.ongojisikbe.domain.board.entity;
+
+import com.example.ongojisikbe.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "게시판")
+public class Board {
+
+    @Id
+    @Column(name = "boardId", nullable = false)
+    private Long boardId;
+
+    @Column(name = "CHAR")
+    private String charField;
+
+    @Column(name = "Field2")
+    private String field2;
+
+    @Column(name = "Field3")
+    private String field3;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/example/ongojisikbe/domain/user/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.ongojisikbe.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "유저")
+public class User {
+
+    @Id
+    @Column(name = "userId", nullable = false)
+    private Long userId;
+
+    @Column(name = "providerId")
+    private String providerId;
+
+    @Column(name = "nickname")
+    private String nickname;
+}
