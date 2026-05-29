247b077e48c985778c8edd4e0e89b81d45d21904
bkh010914@naver.com <bkh010914@naver.com>
2026-05-29 10:34:14 +0900

    fix: objectmapper 수정

--- src/main/java/com/project/ongojisik/global/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/project/ongojisik/global/exception/JwtAuthenticationEntryPoint.java
@@ -1,19 +1,21 @@
 package com.project.ongojisik.global.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(
