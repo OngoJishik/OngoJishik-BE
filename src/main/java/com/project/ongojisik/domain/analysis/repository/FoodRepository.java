950579b1219149dbfa6d569b9dcc04a6d00ea9e6
moonsuho <112683632+moonsuho@users.noreply.github.com>
2026-06-19 10:10:36 +0900

    [Fix] main page api개발 중 누락된 코드 추가 (#70)
    * [Fix] Main Page API제작 시, 누락된 파일(FoodRepository.java) 추가
    
    * [Fix] 메인페이지 api 개발 중 누락된 코드 추가 (#68)

--- src/main/java/com/project/ongojisik/domain/analysis/repository/FoodRepository.java
+++ src/main/java/com/project/ongojisik/domain/analysis/repository/FoodRepository.java
@@ -1,7 +1,9 @@
 package com.project.ongojisik.domain.analysis.repository;
 
 import com.project.ongojisik.domain.analysis.entity.Food;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, String> {
+    List<Food> findByFoodPictureIsNotNullAndFoodPictureNot(String foodPicture);
 }
