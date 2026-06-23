15375f81c7dbafa68899176acfdf2c3640093f81
moonsuho <msh1593@naver.com>
2026-06-23 21:51:41 +0900

    [Fix] 같은 음식명 이미지 재사용 처리

--- src/main/java/com/project/ongojisik/domain/analysis/repository/FoodRepository.java
+++ src/main/java/com/project/ongojisik/domain/analysis/repository/FoodRepository.java
@@ -2,8 +2,16 @@ package com.project.ongojisik.domain.analysis.repository;
 
 import com.project.ongojisik.domain.analysis.entity.Food;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, String> {
     List<Food> findByFoodPictureIsNotNullAndFoodPictureNot(String foodPicture);
+
+    List<Food> findByFoodName(String foodName);
+
+    Optional<Food> findFirstByFoodNameAndFoodPictureIsNotNullAndFoodPictureNotOrderByFoodIdAsc(
+            String foodName,
+            String foodPicture
+    );
 }
